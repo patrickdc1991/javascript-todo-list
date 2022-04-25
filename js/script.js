c796e20357277b55769811df8ae2a43f68d49733
PatrickD_1 <patrickdeclara@hautla.com>
2022-04-25 14:37:30 +1000

    added check and delete

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@ let items = [];
 
 function addItem(){
     let item = document.createElement("div");
+
+    let checkContainer = document.createElement("div");
+    let check = document.createElement("input");
+    check.setAttribute("type","checkbox");
+    checkContainer.appendChild(check);
+
+    let textContainer = document.createElement("div");
     let para = document.createElement("p");
     let text = document.createTextNode("Todo item " + items.length)
     para.appendChild(text);
-    item.appendChild(para);
+    textContainer.appendChild(para);
+
+    let deleteContainer = document.createElement("div");
+    let deletePara = document.createElement("p");
+    let deleteText = document.createTextNode("x")
+    deletePara.appendChild(deleteText);
+    deleteContainer.appendChild(deletePara);
+
+    item.appendChild(checkContainer);
+    item.appendChild(textContainer);
+    item.appendChild(deleteContainer);
+
     document.getElementById("items").appendChild(item);
     items.push(item);
 }
